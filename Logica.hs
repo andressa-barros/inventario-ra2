f2bd0375bd77cbc96659a9c96c4afb23419c4156
Lissa Deguti <lilideguti@gmail.com>
2025-11-09 18:04:21 -0300

    feat: add addItem function to insert new items
    Introduces the addItem function, which adds a new item to the inventory if the item ID does not already exist. Returns an error if the ID exists, otherwise updates the inventory and creates a success log entry.

--- Logica.hs
+++ Logica.hs
@@ -14,3 +14,17 @@ import Data.Time (UTCTime) -- p poder escrever nas assinaturas
 
 -- se retorno de sucesso = (novo inventario + log)
 type ResultadoOperacao = (Inventario, LogEntry)
+
+-- addItem, adiciona um item nv ao inventario
+-- verifica se o ID já existe,
+-- se sim: erro, se nao: insere o item no mapa, cria log de sucesso e retorna Right
+
+addItem :: UTCTime -> Item -> Inventario -> Either String ResultadoOperacao
+addItem horario item inventario = case Map.lookup (itemID item) inventario of
+    Just _ -> Left "id já existe"
+    Nothing ->
+        let novoInventario = Map.insert (itemID item) item inventario
+            logEntrada = LogEntry horario Add ("add" ++ show (quantidade item) ++ "x " ++ itemID item) Sucesso
+        in Right (novoInventario, logEntrada)
+
+        
