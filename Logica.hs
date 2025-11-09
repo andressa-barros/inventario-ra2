a8e35f1ea3ab53269e92c0a0e8e98015b1a28413
Lissa Deguti <lilideguti@gmail.com>
2025-11-09 18:15:49 -0300

    feat: add removeItem function to inventory logic
    Implements the removeItem function, allowing removal of a specified quantity of an item from the inventory. The function checks for item existence, validates the quantity, updates the inventory, and logs the operation.

--- Logica.hs
+++ Logica.hs
@@ -16,7 +16,7 @@ import Data.Time (UTCTime) -- p poder escrever nas assinaturas
 type ResultadoOperacao = (Inventario, LogEntry)
 
 -- addItem, adiciona um item nv ao inventario
--- verifica se o ID já existe,
+-- verifica se o ID já existe:
 -- se sim: erro, se nao: insere o item no mapa, cria log de sucesso e retorna Right
 
 addItem :: UTCTime -> Item -> Inventario -> Either String ResultadoOperacao
@@ -27,4 +27,20 @@ addItem horario item inventario = case Map.lookup (itemID item) inventario of
             logEntrada = LogEntry horario Add ("add" ++ show (quantidade item) ++ "x " ++ itemID item) Sucesso
         in Right (novoInventario, logEntrada)
 
-        
+-- removeItem, remove certa qntd de um item
+-- Map.lookup acha item:
+-- se n achar: erro, se achar: confere se qntd > 0 e < que estoque atual,
+-- se td certo: cria nv versao do item com qntd reduzida, att mapa e monta log
+
+
+removeItem :: UTCTime -> String -> Int -> Inventario -> Either String ResultadoOperacao
+removeItem horario itemId quantidadeRemovida inventario = case Map.lookup itemId inventario of
+    Nothing -> Left "id não encontrado"
+    Just item ->
+        if quantidadeRemovida <= 0 then Left "quantidade invalida"
+        else if quantidadeRemovida > quantidade item then Left "estoque insuficiente"
+        else
+            let itemAtualizado = item { quantidade = quantidade item - quantidadeRemovida }
+                novoInventario = Map.insert itemId itemAtualizado inventario
+                logEntrada = LogEntry horario Remove ("remove " ++ show quantidadeRemovida ++ "x " ++ itemId) Sucesso
+            in Right (novoInventario, logEntrada)
