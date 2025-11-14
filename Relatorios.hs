425195b018e959b8c9ca724ffa7dac74267ab134
Fernanda Moraes <85085303+Fer9211@users.noreply.github.com>
2025-11-14 16:35:43 -0300

    Refactor log filtering and item tracking functions

--- Relatorios.hs
+++ Relatorios.hs
@@ -5,21 +5,38 @@ module Relatorios
   ) where
 
 import Tipos
-import Data.List (sort, group, maximumBy)
-import Data.Ord (comparing)
+import Data.List (sort, group)
 
+-- Filtra os logs que têm o ID do item no texto
 historicoPorItem :: String -> [LogEntry] -> [LogEntry]
-historicoPorItem itemID = filter (\logEntry -> itemID `elem` words (detalhes logEntry))
+historicoPorItem idItem logs =
+  filter (\l -> idItem `elem` words (detalhes l)) logs
 
+-- Retorna apenas os logs que são falhas
 logsDeErro :: [LogEntry] -> [LogEntry]
-logsDeErro = filter (\logEntry -> case status logEntry of
-                        Falha _ -> True
-                        _ -> False)
+logsDeErro logs =
+  filter (\l -> case status l of
+                  Falha _ -> True
+                  _       -> False) logs
 
+-- Encontra o item mais movimentado
 itemMaisMovimentado :: [LogEntry] -> Maybe (String, Int)
 itemMaisMovimentado logs =
-  let 
-      logsDeSucesso = filter (\l -> case status l of { Sucesso -> True; _ -> False }) logs
-      nomes = [last (words (detalhes l)) | l <- logsDeSucesso, let ws = words (detalhes l), not (null ws)]
-      agrupado = map (\xs -> (head xs, length xs)) . group . sort $ nomes
-  in if null agrupado then Nothing else Just (maximumBy (comparing snd) agrupado)
+  let
+      -- só pega logs que deram certo
+      okLogs = filter (\l -> case status l of Sucesso -> True; _ -> False) logs
+
+      -- pega a última palavra do texto (que é o nome do item)
+      nomes = [ last (words (detalhes l))
+              | l <- okLogs
+              , not (null (words (detalhes l)))
+              ]
+
+      -- agrupa iguais e conta
+      agrupado = map (\x -> (head x, length x)) (group (sort nomes))
+
+      -- função simples para pegar o maior
+      pegaMaior [] = Nothing
+      pegaMaior xs = Just (foldl1 (\a b -> if snd a >= snd b then a else b) xs)
+
+  in pegaMaior agrupado
